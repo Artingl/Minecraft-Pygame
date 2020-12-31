77fa4e72fd7c666f945ab3e8ba16476447e84de8
Artyom Troyazykov <mrheldit2@gmail.com>
2020-12-31 17:27:06 +0700

    Particles update

--- game/Particles.py
+++ game/Particles.py
@@ -18,7 +18,6 @@ class Particles:
                 numbers = range(-5, 5)
 
         for i in range(count):
-
             dx, dy, dz = choice(numbers), choice(numbers), choice(numbers)
             self.particles.append([list(p), cubeClass, 0.2, [dx, dy, dz], .001, direction])
 
@@ -57,10 +56,29 @@ class Particles:
                 i[0][0] += i[3][0] / 50
                 i[0][2] += i[3][2] / 50
 
-            self.gl.particleBatch.add(24,
-                                      GL_QUADS,
-                                      i[1].t[1],
-                                      ('v3f', flatten(cube_vertices((i[0][0], i[0][1], i[0][2]), i[2]))))
+            # self.gl.particleBatch.add(24,
+            #                          GL_QUADS,
+            #                          i[1].t[1],
+            #                          ('v3f', flatten(cube_vertices((i[0][0], i[0][1], i[0][2]), i[2]))))
+            x, y, z = tuple(i[0])
+            X, Y, Z = x + i[2], y + i[2], z + i[2]
+
+            tex_coords = ('t2f', (0, 0, 1, 0, 1, 1, 0, 1))
+
+            self.gl.particleBatch.add(4, GL_QUADS, i[1].t[4], ('v3f', (X, y, z, x, y, z, x, Y, z, X, Y, z)),
+                                      tex_coords)  # back
+            self.gl.particleBatch.add(4, GL_QUADS, i[1].t[5], ('v3f', (x, y, Z, X, y, Z, X, Y, Z, x, Y, Z)),
+                                      tex_coords)  # front
+
+            self.gl.particleBatch.add(4, GL_QUADS, i[1].t[0], ('v3f', (x, y, z, x, y, Z, x, Y, Z, x, Y, z)),
+                                      tex_coords)  # left
+            self.gl.particleBatch.add(4, GL_QUADS, i[1].t[1], ('v3f', (X, y, Z, X, y, z, X, Y, z, X, Y, Z)),
+                                      tex_coords)  # right
+
+            self.gl.particleBatch.add(4, GL_QUADS, i[1].t[2], ('v3f', (x, y, z, X, y, z, X, y, Z, x, y, Z)),
+                                      tex_coords)  # bottom
+            self.gl.particleBatch.add(4, GL_QUADS, i[1].t[3], ('v3f', (x, Y, Z, X, Y, Z, X, Y, z, x, Y, z)),
+                                      tex_coords)  # top
 
             i[2] -= 0.01
             self.particles[e] = i
